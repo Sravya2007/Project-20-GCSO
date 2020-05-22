f5740dade523c75deb169ae0ac20655c115c2c17
Sravya2007 <65549677+Sravya2007@users.noreply.github.com>
2020-05-22 17:10:52 +0530

    isTouching change

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ function setup() {
   car = createSprite(50, 200, 50, 50);
 
   wall = createSprite(1500, 200, 60, height/2);
+  
   wall.shapeColor = color(80, 80, 80);
 }
 
@@ -19,7 +20,7 @@ function draw() {
   
   car.velocityX = speed;
 
-  if(wall.x - car.x < (car.width + wall.width/2)){
+  if(wall.x - car.x < (car.width + wall.width)/2){
     car.velocityX = 0;
 
     var deformation = 0.5 * weight * speed * speed/22509;
